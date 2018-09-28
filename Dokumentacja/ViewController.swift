640e1bf917c0ab417f00477823bc9db45771c7af
Damian Wozniak <damian.pwozniak@gmail.com>
2018-09-28 11:20:33 +0200

    Initial Commit

---
+++ Dokumentacja/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dokumentacja
+//
+//  Created by Peter on 28.09.2018.
+//  Copyright © 2018 Wozniakus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
